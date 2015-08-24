48f3358d3bfa0c19e010c935ce1f6f3892c745b8
weijefffrey <jeffreycswei@gmail.com>
2015-08-24 19:49:23 +0800

    fix(master): fix schemea

--- db/schema.rb
+++ db/schema.rb
@@ -71,35 +71,6 @@ ActiveRecord::Schema.define(version: 20150824110052) do
   add_index "biz_users", ["reset_password_token"], name: "index_biz_users_on_reset_password_token", unique: true, using: :btree
   add_index "biz_users", ["uid", "provider"], name: "index_biz_users_on_uid_and_provider", unique: true, using: :btree
 
-  create_table "business_users", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "phone_number"
-    t.string   "email"
-    t.text     "address"
-    t.string   "password"
-    t.string   "confirm_password"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
-  create_table "companies", force: :cascade do |t|
-    t.string   "name"
-    t.string   "industry"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "individual_users", force: :cascade do |t|
-    t.string   "email"
-    t.integer  "phone_number"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "password"
-    t.string   "confirm_password"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
   create_table "services", force: :cascade do |t|
     t.string   "service_name"
     t.datetime "created_at",   null: false
